4320b231c412a3ea44a0281e3ddf99e8e9d05a97
ismaila5623 <iscomaila5623@gmail.com>
2023-02-17 02:53:33 -0800

    reduced character number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,18 +13,18 @@
 int main(void)
 {
 	int n;
-	int lastDigit;
+	int lastD;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	lastDigit = n % 10;
+	lastD = n % 10;
 
-	if (lastDigit > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
-	else if (lastDigit == 0)
-		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
-	else if (lastDigit < 6 && lastDigit != 0)
-		printf("Last digit of %d is %d and i sless than 6 and not 0\n", n, lastDigit);
+	if (lastD > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastD);
+	else if (lastD == 0)
+		printf("Last digit of %d is %d and is 0\n", n, lastD);
+	else if (lastD < 6 && lastD != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastD);
 	return (0);
 }
