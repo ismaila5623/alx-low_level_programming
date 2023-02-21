7645a6f00aed31066e604b64c2729992a33a6230
ismaila5623 <iscomaila5623@gmail.com>
2023-02-21 07:11:48 -0800

    prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar and a new line
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	_putchar('a');
+	_putchar('w');
+	_putchar('+');
+	_putchar('\n');
+
+	return (0);
+}
