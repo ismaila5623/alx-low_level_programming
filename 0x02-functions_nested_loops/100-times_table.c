b1efb24b833bdb7edb9a359db936b32cd961d18b
ismaila5623 <iscomaila5623@gmail.com>
2023-02-21 08:01:05 -0800

    100 times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints n times table
+ *
+ * Return: Always 0.
+ */
+
+void print_times_table(int n)
+{
+	int num, mult, prd;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (num = 0; num <= n; num++)
+		{
+			_putchar('0');
+			for (mult = 1; mult <= n; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				prd = num * mult;
+				if (prd <= 99)
+					_putchar(' ');
+				if (prd <= 9)
+					_putchar(' ');
+				if (prd >= 100)
+					_putchar((prd / 100) + '0');
+					_putchar(((prd / 10)) % 10 + '0');
+			}
+			else if (prd <= 99 && prd >= 10)
+			{
+				_putchar((prd / 10) + '0');
+			}
+			_putchar((prd % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
