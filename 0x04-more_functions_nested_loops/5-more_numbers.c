fb1e4666ab70a63446ad9ab453a9dd54dc12d9cb
ismaila5623 <iscomaila5623@gmail.com>
2023-02-23 04:37:02 -0800

    betty style

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,15 +1,18 @@
 #include "main.h"
 
 /**
- * more_numbers: prints numbers from 0 to 14 upto 10 times
- *
+ * more_numbers - prints numbers from 0 to 14 upto 10 times
+ *Return: prints 0 to 14 upto 10 times
  */
 
 void more_numbers(void)
 {
-	for (int i = 0; i < 10; i++)
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
 	{
-		for (int j = 0; j <= 14; j++)
+		for (j = 0; j < 15; j++)
 		{
 			_putchar(j + '0');
 		}
