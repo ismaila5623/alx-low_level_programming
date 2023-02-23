49f13170e87a712e3882b157217254a0e136b619
ismaila5623 <iscomaila5623@gmail.com>
2023-02-23 02:35:46 -0800

    prints numbers from 0 to 14 upto x10

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * more_numbers: prints numbers from 0 to 14 upto 10 times
+ *
+ */
+
+void more_numbers(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 14; j++)
+		{
+			_putchar(j + '0');
+		}
+		_putchar('\n');
+	}
+}
