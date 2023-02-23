021b9062a9ac3e9e180e97f129832d42b3961f0e
ismaila5623 <iscomaila5623@gmail.com>
2023-02-23 02:39:09 -0800

    prints square on terminal

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square: prints a square using the char #
+ *
+ */
+
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
