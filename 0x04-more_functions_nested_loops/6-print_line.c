9680cf704e93fe8d23bf53c88bab7988b4c50435
ismaila5623 <iscomaila5623@gmail.com>
2023-02-23 04:42:56 -0800

    fix betty style

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,13 +1,16 @@
 #include "main.h"
 
 /**
- * print_line: draws a straight line by certain number of times with _ char
- *
+ * print_line - draws a straight line by certain number of times with _ char
+ *@n: number of size
+ *Return: prints straight line n number of times
  */
 
 void print_line(int n)
 {
-	for (int i = 0; i < n; i++)
+	int i;
+
+	for (i = 0; i < n; i++)
 		_putchar('_');
 	_putchar('\n');
 }
