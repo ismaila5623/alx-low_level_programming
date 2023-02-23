750d3f2f75abc274ec7fdf1b3e680b5b08182c0e
ismaila5623 <iscomaila5623@gmail.com>
2023-02-23 06:41:57 -0800

    fix error

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,15 +12,11 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 	
-	if (a > b && a > c)
+	if (a > b && b > c)
 	{
 		largest = a;
 	}
-	else if (a > b && c > a)
-	{
-		largest = c;
-	}
-	else if (b > c)
+	else if (b > a && a > c)
 	{
 		largest = b;
 	}
