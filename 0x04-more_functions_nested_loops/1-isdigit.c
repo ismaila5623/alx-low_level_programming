ce5067a42e2ea1342066132e2c31a650157b2be9
ismaila5623 <iscomaila5623@gmail.com>
2023-02-23 02:48:44 -0800

    fix betty style error

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,9 +2,9 @@
 #include <ctype.h>
 
 /**
- * _isdigit: checks if a character is a digit or not
- *
- * Return: 0
+ * _isdigit - checks if a character is a digit or not
+ *@c: the char to be checked
+ * Return: 1 if digit and 0 if not
  */
 
 int _isdigit(int c)
