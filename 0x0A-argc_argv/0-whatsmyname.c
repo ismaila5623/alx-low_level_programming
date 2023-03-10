1236543c72e0d10955bd2fafba63e8705d79eb1e
ismaila5623 <iscomaila5623@gmail.com>
2023-03-10 02:34:40 -0800

    first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main entry
+ *@argc: argument count
+ *@argv: argument vector
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc]);
+
+	return (0);
+}
