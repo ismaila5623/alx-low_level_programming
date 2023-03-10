e53a47852947638b531aad9f86c1b57a8b366038
ismaila5623 <iscomaila5623@gmail.com>
2023-03-10 03:13:12 -0800

    converts str to int

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - main entry
@@ -17,7 +18,7 @@ int main(int argc, char *argv[])
 	{
 		for (; i < argc; i++)
 		{
-			result = result * argv[i];
+			result = result * atoi(argv[i]);
 		}
 		printf("%d\n", result);
 	}
