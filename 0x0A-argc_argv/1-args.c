a4d3f6e2113deb8b3f27ea27f79263bae6654629
ismaila5623 <iscomaila5623@gmail.com>
2023-03-10 02:51:40 -0800

    1-args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main entry
+ *@argc: argument count
+ *@argv: argument vector
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
