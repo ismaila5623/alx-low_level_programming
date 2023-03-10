c20b5f70bf11fd8864af13457d93839a8aef68bf
ismaila5623 <iscomaila5623@gmail.com>
2023-03-10 02:49:53 -0800

    fix error

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,9 @@
  * Return: Always 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", argv[argc]);
+	printf("%s\n", *argv);
 
 	return (0);
 }
